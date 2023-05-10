5e43828218f8209fcf3f271573dd8ec41907bf62
Hongjun <ggaye159@naver.com>
2023-05-10 16:03:31 +0900

    init. 통합 테스트 함수 설정

---
+++ src/Test/integration_test.py
@@ -0,0 +1,33 @@
+def receive_message_Test():
+    # Kiwoomi 봇으로부터 메시지를 받아오는 코드
+    return
+
+def analyze_message_Test(message):
+    # Controller.py를 호출하여 메시지를 분석하는 코드
+    return
+
+def get_stock_info_Test(stock_code):
+    # 키움 오픈API를 이용하여 해당 주식 정보를 조회하는 코드
+    return
+
+def get_news_Test():
+    # 셀레니움을 이용하여 뉴스를 크롤링하는 코드
+    return
+
+def search_data_Test(keyword):
+    # 키움 오픈API를 이용하여 검색 결과를 조회하는 코드
+    return
+
+def set_alarm_Test(stock_code, price):
+    # 키움 오픈API를 이용하여 알람을 설정하는 코드
+    return
+
+def send_message_Test(message, chat_id):
+    # Kiwoomi 봇으로 메시지를 전송하는 코드
+    return
+
+def test_scenario_Test():
+    # 위에서 작성한 함수들을 조합하여 전체 테스트 시나리오를 실행하는 코드
+    return
+
+
