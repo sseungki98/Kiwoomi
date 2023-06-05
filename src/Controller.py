4d6a44716b9675992906b88faadb08dc38c42313
sseungki98 <appp_le@naver.com>
2023-06-05 13:00:55 +0900

    feat: 키움 api 호출에 필요한 Controller 최종 업데이트(alarm)

--- src/Controller.py
+++ src/Controller.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-from pykiwoom.kiwoom import Kiwoom
+from pykiwoom.kiwoom import *
+from PyQt5.QtCore import *
+from PyQt5.QtTest import *
 import KiwoomConnector
-import os,json
+import os, json
+import time
 
 kiwoom = Kiwoom()
 kiwoom.CommConnect(block=True)  # 키움 API 접속
@@ -11,18 +14,18 @@ kiwoom.CommConnect(block=True)  # 키움 API 접속
 def bring_info(name):
     if not (os.path.exists('./stock_data.json')):
        KiwoomConnector.get_codes()
-    
+
     with open('./stock_data.json','r') as file:
         stock_dict=json.load(file)
         stock_code=stock_dict.get(name)
     if stock_code is None:
         return("존재하지 않는 주식명 입니다. 확인후 다시 시도해주세요")
-    
+
     value_list = kiwoom.block_request("opt10001",
                                       종목코드=stock_code,
                                       output="주식기본정보",
                                       next=0).to_dict(orient='records')
-    
+
     print(value_list)
     return_text = ''
     print(return_text)
@@ -49,10 +52,22 @@ def bring_News(messages):
     return texts
 
 def set_Alarm(messages):
-    return True
+    class KiwoomApiCallback(QObject):
+        def __init__(self):
+            super().__init__()
 
-def search_info(messages):
+        @pyqtSlot()
+        def receive_tr_data(self):
+            if self.price > messages[1]:
+                now = time.strftime('%Y.%m.%d - %H:%M:%S')
+                return_string = "주식명: " + messages[0] + '\n' + messages[1] + '원에 도달했습니다.\n' + now
+                return return_string
 
-    return True
+    callback = KiwoomApiCallback()
+    kiwoom.OnReceiveRealData.connect(callback.receive_tr_data)  # 실시간 데이터 수신 시그널 연결
+
+    kiwoom.SetRealReg("0",messages[1], "20;10", "0")
+
+
 
 
