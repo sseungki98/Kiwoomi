268d4d9ded87f5462886255686335251ac897283
sseungki98 <appp_le@naver.com>
2023-06-05 12:59:44 +0900

    feat: Telegram과 통신에 필요한 botCommunicator 최종 업데이트

--- src/botCommunicator.py
+++ src/botCommunicator.py
@@ -2,6 +2,8 @@ import telegram
 from telegram.ext import Updater, CommandHandler
 from telegram.ext import MessageHandler, Filters
 import Controller
+from datetime import datetime
+import time
 
 # 텔레그램 봇 토큰
 TOKEN = '6184520289:AAH_E_DLW-SZIfFkLrnDv9sRrUvWOTJVw68'
@@ -16,23 +18,23 @@ def start(update, context):
 #/help 명령어 처리 함수
 def help(update, context):
     chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text='키우미 봇은 간단한 주식 도움 봇이에요 /r \
-                             /info [주식명]= 주식의 현재 시세 및 관련 정보를 불러옵니다. /r\
-                             /search [주식명][옵션]= 주식의 일봉,월봉,연봉 등 특정 정보를 불러옵니다./r \
-                             /alarm [주식명][가격]=주식이 특정 가격에 도달하였을 경우 봇을 통해 알려줍니다. /r\
-                             /news [주식명]=해당 주식에 관련된 기사를 모으고 헤드라인 기사를 띄워줍니다. /r')
+    context.bot.send_message(chat_id=chat_id, text='키우미 봇은 간단한 주식 도움 봇이에요\n/info [주식명]= 주식의 현재 시세 및 관련 정보를 불러옵니다.\n/search [주식명][옵션]= 주식의 일봉,월봉,연봉 등 특정 정보를 불러옵니다.\n/alarm [주식명][가격]=주식이 특정 가격에 도달하였을 경우 봇을 통해 알려줍니다.\n/news [주식명]=해당 주식에 관련된 기사를 모으고 헤드라인 기사를 띄워줍니다.')
 
 # /info 명령어 처리 함수
 def info(update, context):
     chat_id = update.effective_chat.id
-    print(context.args[0])
-    messages.append(context.args[0])
+    if len(context.args) == 1:
+        messages.append(context.args[0])
+    else:
+        for i in range(len(context.args)):
+            messages.append(context.args[i])
+
     print(messages)
     # 메시지를 배열에 저장
     # Controller.py의 함수 호출
     result = Controller.bring_info(messages)
-    context.bot.sendMessage(chat_id=chat_id, text=messages[0] + '에 관한 정보입니다')
-    context.bot.sendMessage(chat_id=chat_id, text=result)
+    bot.sendMessage(chat_id=chat_id, text=messages[0] + '에 관한 정보입니다')
+    bot.sendMessage(chat_id=chat_id, text=result)
     messages.clear()
 
 
@@ -54,13 +56,14 @@ def alarm(update, context):
     
     # 메시지를 배열에 저장
     messages.append(context.args[0])
-    messages.append(context.args[1])
     # Controller.py의 함수 호출
-    result=Controller.set_Alarm(messages)
+    result = Controller.set_Alarm(messages)
     if(result==True):
-        bot.sendMessage(chat_id=chat_id, text=context.args[0] + '에 대한 지정가 알람을 완료하였습니다.')
+        bot.sendMessage(chat_id=chat_id, text=context.args[0] + '에 대한 지정가 알람을 완료하였습니다.(' + context.args[1] + '원)')
     else:
         bot.sendMessage(chat_id=chat_id, text=' 알람 등록에 실패 하였습니다. 주식명을 확인하여주세요.')
+
+    bot.sendMessage(chat_id=chat_id, text=result)
 
 # /search 명령어 처리 함수
 def search(update, context):
